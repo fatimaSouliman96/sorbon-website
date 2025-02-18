e092221bdfe1dca47d56cecc025f869cae20a027
fatimaSouliman96 <fatima96souliman@gmail.com>
2025-02-18 13:56:31 +0200

    edit

--- components/cardCity/CardCity.tsx
+++ components/cardCity/CardCity.tsx
@@ -5,14 +5,14 @@ export default function CardCity() {
   return (
     <div className="relative h-[200px] rounded-lg " >
             <div className=" w-full h-full ">
-                <Image className="rounded-lg" fill src={"/city.jfif"} alt="category" />
+                <Image className="rounded-lg" fill src="/city.jfif" alt="category" />
                 <div className="absolute rounded-lg w-full h-full z-0 bg-[#17133FE5] top-0 left-0">
 
                 </div>
             </div>
             <div className="flex items-center justify-center gap-2 absolute  w-full h-full z-1 top-[40%] left-[4%] " >
                     <p className="text-base font-medium text-white" >Madrid - Spain</p>
-                    <Image src={"/arrow-white.svg"} alt="arrow-eylow" width={6} height={12} />
+                    <Image src="/arrow-white.svg" alt="arrow-eylow" width={6} height={12} />
             </div>
         </div>
   )
