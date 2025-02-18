81f5a189dfeee23087ba9a72bf15502ca8178143
fatimaSouliman96 <fatima96souliman@gmail.com>
2025-02-18 13:58:09 +0200

    edit

--- components/vuneusSection/Veunes.tsx
+++ components/vuneusSection/Veunes.tsx
@@ -3,7 +3,7 @@ import VeunesSlider from '../veunesSlider/VeunesSlider'
 
 export default function Veunes() {
   return (
-    <div className='veunes bg-[#f0effa]  max-w-full px-12 py-4' >
+    <div className='veunes bg-[#f0effa]  w-full px-12 py-4' >
         <h1 className='text-4xl font-semibold text-secondary' >Training Venues</h1>
         <VeunesSlider/>
     </div>
